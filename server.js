07018c84c40d19a4738cdcd844b06b1455ebf164
shining-star123 <johnathantafoya88@gmail.com>
2025-03-24 20:07:05 -0500

    Finalize - DB and Server

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+app.post('/users', (req, res) => {
+    const newUser = new User(req.body);
+
+    newUser.save()
+        .then(user => {
+            res.send('success');
+        }).catch(err => {
+            res.status(404).send(err);
+        })
+})
+
 app.get('/users/:id', async (req, res) => {
     const { id } = req.params;
 
     try {
-        const user = await user.findOne({ _id: id, age: { $gt: 21 } });
+        const user = await User.findOne({ _id: id, age: { $gt: 21 } });
 
         if (!user) {
             return res.status(404).json({ message: 'User not found or age is 21 or below' });
