d86596e7f198740f7e1bfe234167c75a64134a41
shining-star123 <johnathantafoya88@gmail.com>
2025-03-24 19:56:31 -0500

    Express Router Complete

--- server.js
+++ server.js
@@ -9,10 +9,29 @@ const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+app.get('/users/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await user.findOne({ _id: id, age: { $gt: 21 } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found or age is 21 or below' });
+        }
+
+        res.json(user);
+    } catch (err) {
+        if (err.name === 'CastError') {
+            return res.status(400).json({ message: 'Invalid user ID format' });
+        }
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
